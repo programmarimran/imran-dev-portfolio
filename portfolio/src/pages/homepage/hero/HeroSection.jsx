8c6e94c95c0c71bce09e63e9bb609af0bc1235c7
programmarimran <programmarimran@gmail.com>
2025-06-30 12:09:56 +0600

    added a modern animation in the hero section

--- portfolio/src/pages/homepage/hero/HeroSection.jsx
+++ portfolio/src/pages/homepage/hero/HeroSection.jsx
@@ -5,9 +5,12 @@ const HeroSection = () => {
   return (
     <section className="max-h-[1000px] my-12 border-4 rounded-2xl border-[#a878d8]  flex flex-col-reverse md:flex-row items-center justify-between gap-10 px-6 md:px-16 py-12  text-white">
       {/* Left content */}
-      <div data-aos="fade-right"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine" className="w-full md:w-1/2 flex flex-col gap-6">
+      <div
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+        className="w-full md:w-1/2 flex flex-col gap-6"
+      >
         <h1 className="text-4xl md:text-3xl font-bold leading-tight text-shadow">
           Hi, I’m <span className="text-primary">Md Imran Hasan</span>
         </h1>
@@ -15,6 +18,12 @@ const HeroSection = () => {
         <h2 className="text-xl text-shadow font-medium text-gray-300">
           React & MERN Stack Developer
         </h2>
+        <h1 align="center">
+          <img
+            src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=28&duration=4000&pause=1000&color=F76C6C&width=800&lines=Assalamu+Alaikum+👋+I'm+Md+Imran+Hasan;MERN+Stack+Developer+from+Bangladesh;React+%7C+Node+%7C+Express+%7C+MongoDB;Always+learning+%7C+Open+to+Work"
+            alt="Typing SVG"
+          />
+        </h1>
 
         <p className="text-base text-shadow text-gray-300">
           I build modern web apps using MongoDB, Express, React, and Node.js. I
